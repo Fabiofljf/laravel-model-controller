e3bf469bcdb929f021d020aae96c7a5fcf5cbdc5
FabioLaForesta <fabiolaforesta@icloud.com>
2022-06-13 15:49:42 +0200

    Add database

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+// - questo è il model.
 use App\movie;
 
 use Illuminate\Http\Request;
@@ -9,6 +10,7 @@ use Illuminate\Http\Request;
 class MovieController extends Controller
 {
     public function Movie(){
+        // - all() è metodo statico con il quale prendiamo tutto ciò che si trova nella tabella movies.
         $movies = movie::all();
         dd($movies);
         return view('index');
