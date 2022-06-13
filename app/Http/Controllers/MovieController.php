4841e7ab3dc3866b04c32f0d4899af82ab196214
FabioLaForesta <fabiolaforesta@icloud.com>
2022-06-13 15:47:42 +0200

    Add database

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\movie;
+
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     public function Movie(){
-        
+        $movies = movie::all();
+        dd($movies);
         return view('index');
     }
 }
