2de729219138c69ae8913d83f1f3c00ad902f23e
FabioLaForesta <fabiolaforesta@icloud.com>
2022-06-13 15:38:37 +0200

    Add controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Movie;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// - Rimandato a App/Http/Controllers
+Route::get('/', 'MovieController@Movie');
